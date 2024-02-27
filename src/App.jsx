3456fab3de1100a9b8dde0eb16947f950cd0a4c0
swis saklo <swissaklo@gmail.com>
2024-02-27 18:06:50 +0530

    Add react-router-dom and create new pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Profile from './pages/profile'
+import About from './pages/About'
+import Home from './pages/Home'
 const App = () => {
   return (
-    <div>
-      
-    </div>
+   <BrowserRouter>
+   <Routes>
+     <Route path="/" element={<Home/>}/>
+     <Route path="/sign-in" element={<SignIn/>}/>
+     <Route path="/sign-up" element={<SignUp/>}/>
+     <Route path="/profile" element={<Profile/>}/>
+     <Route path="/about" element={<About/>}/>
+     
+   </Routes>
+   
+   </BrowserRouter>
   )
 }
 
