c01355b493ca9cf905742d5dec630542e73e4a12
swis saklo <swissaklo@gmail.com>
2024-02-27 18:41:43 +0530

    Added Header

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import SignIn from './pages/SignIn'
 import Profile from './pages/profile'
 import About from './pages/About'
 import Home from './pages/Home'
+import Header from './components/Header'
 const App = () => {
   return (
    <BrowserRouter>
+   <Header>
    <Routes>
      <Route path="/" element={<Home/>}/>
      <Route path="/sign-in" element={<SignIn/>}/>
@@ -16,6 +18,8 @@ const App = () => {
      <Route path="/about" element={<About/>}/>
      
    </Routes>
+   </Header>
+   
    
    </BrowserRouter>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import {FaSearch} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+export default function Header()  {
+  return (
+    <header className='bg-slate-300 shadow-lg'>
+   <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+   <Link to='/'>
+   <h1 className='font-bold text-sm sm:text-3xl flex flex-wrap'>
+        <span   className='text-blue-400'>Dream</span>
+        <span className='text-slate-700'>Estate</span>
+    </h1>
+   </Link>
+    <form className='bg-slate-100 p-3 rounded-lg flex'>
+      <input type="text" placeholder='Search.....' className="bg-transparent focus:outline-none w-24 sm:w-64"/>
+      <FaSearch className='bg-slate-500 bg-transparent mt-1'/>
+    </form>
+    <ul className='flex gap-3'>
+      <Link to='/'>
+      <li className='hidden sm:inline text-slate-800 hover:underline'>Home</li>
+      </Link>
+<Link to='/about'>
+      <li className='hidden sm:inline text-slate-800 hover:underline'>About</li>
+</Link>
+<Link to ='/sign-in'>
+      <li className='hidden sm:inline text-slate-800 hover:underline'>Sign In</li>
+</Link>
+    </ul>
+    {/* <div>
+      asdad
+    </div> */}
+    
+    </div>   
+
+    </header>
+  )
+}
+
+
