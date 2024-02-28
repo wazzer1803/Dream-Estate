be7e2fcc51b513ff78142528317b85cfddc065ce
swis saklo <swissaklo@gmail.com>
2024-02-28 19:34:50 +0530

    update and delete lisiting added

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Home from './pages/Home'
 import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing.jsx'
+import UpdateListing from './pages/UpdateListing.jsx'
 export default function App(){
   return (
    <BrowserRouter>
@@ -19,6 +20,7 @@ export default function App(){
      <Route element={<PrivateRoute/>}>
      <Route path="/profile" element={<Profile/>}/>
      <Route path="/create-listing" element={<CreateListing/>}/>
+     <Route path="/update-listing/:id"element={<UpdateListing/>}/>
      </Route>
      <Route path="/about" element={<About/>}/>
      
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -124,10 +124,44 @@ export default function Profile() {
       dispatch(deleteUserFailure(error.message));
     }
   };
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <div  className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <h1 className='text-3xl font-semibold  text-center my-7'>Profile</h1>
       <form className='flex flex-col  gap-4 ' onSubmit={handleSubmit}>
       <input
           onChange={(e) => setFile(e.target.files[0])}
@@ -136,7 +170,7 @@ export default function Profile() {
           hidden
           accept='image/*'
         />
-        <img    onClick={() => fileRef.current.click()} src ={formData.avatar||currentUser.avatar}alt="profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"/>
+        <img    onClick={() => fileRef.current.click()} src ={formData.avatar||currentUser.avatar}alt="profile" className="rounded-full h-28 w-28 object-cover cursor-pointer self-center mt-2"/>
         <p className='text-sm self-center'>
           {fileUploadError ? (
             <span className='text-red-700'>
@@ -175,35 +209,77 @@ export default function Profile() {
         />
          <button
           disabled={loading}
-          className="bg-slate-900 text-white p-3 rounded-lg hover:opacity-95 disabled opacity:80"
+          className="bg-slate-900  text-white p-3 rounded-lg hover:opacity-95 disabled opacity:80"
         >
-          {loading ? 'Loading...' : 'Update'}
+          {loading ? 'LOADING...' : 'UPDATE'}
         </button>
         <Link   to="/create-listing" className='bg-green-600 text-white p-3 rounded-lg hover:opacity-95 disabled opacity:80 text-center'>
-       Ayo
+      CREATE LISTINGS
         </Link>
       </form>
-      <div className='flex flex-col lg:flex-row justify-between max-w-1000px mt-5  flex-1'>
-       <button disabled={loading} className='bg-red-500 text-bold text-white rounded-lg p-3 hover:opacity-95 disabled opacity:80 pl-11 pr-11' onClick={handleDeleteUser}>
-
-        <span
+      <div className='flex flex-row justify-between max-w-1000px mt-5  flex-1'>
+      <span disabled={loading} onClick={handleDeleteUser}
           
-          className=' cursor-pointer'
+          className=' cursor-pointer text-lg  text-red-500'
           
           >
           Delete account
         </span>
-      </button>
-      <button disabled={loading} onClick={handleSignOut} className='bg-red-500 text-bold text-white rounded-lg p-3 hover:opacity-95 disabled opacity:80 ' >
-        <span className=' cursor-pointer pl-14 pr-14' >
+      
+  <span className=' cursor-pointer  text-red-500 text-lg 'onClick={handleSignOut} >
           Sign out
-        </span>
-      </button>
+        
+      </span>
       </div>
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
       <p className='text-green-700 mt-5'>
         {updateSuccess ? 'User is updated successfully!' : ''}
       </p>
+      <button onClick={handleShowListings} className='text-green-700 w-full text-lg'>
+        Show Listings
+      </button>
+      <p className='text-red-700 mt-5'>
+        {showListingsError ? 'Error showing listings' : ''}
+      </p>
+      {userListings && userListings.length > 0 && (
+        <div className='flex flex-col gap-4 border pl-3 pr-3 rounded-lg border-opacity-90'>
+          <h1 className='text-center mt-7 text-2xl font-semibold '>
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className='border rounded-lg p-3 flex justify-between items-center gap-4'
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt='listing cover'
+                  className='h-24 w-24 object-contain'
+                />
+              </Link>
+              <Link
+                className='text-slate-700 font-bold hover:underline truncate flex-1'
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+
+              <div className='flex flex-col item-center'>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className='text-red-700 uppercase'
+                >
+                  Delete
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className='text-green-700 uppercase'>Edit</button>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
