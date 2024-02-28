e0e9026e21129cd5ba5a49e88d08a48aafbfcb3d
swis saklo <swissaklo@gmail.com>
2024-02-29 00:37:44 +0530

    Implemeted search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing.jsx'
 import UpdateListing from './pages/UpdateListing.jsx'
 import Listing from './pages/Listing.jsx'
+import Search from './pages/Search.jsx'
 export default function App(){
   return (
    <BrowserRouter>
@@ -19,7 +20,9 @@ export default function App(){
      <Route path="/sign-in" element={<SignIn/>}/>
      <Route path="/sign-up" element={<SignUp/>}/>
       <Route path="listing/:id" element={<Listing/>}/>
+     <Route path="/search" element={<Search/>}/>
      <Route element={<PrivateRoute/>}>
+
      <Route path="/profile" element={<Profile/>}/>
      <Route path="/create-listing" element={<CreateListing/>}/>
      <Route path="/update-listing/:id"element={<UpdateListing/>}/>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,28 @@
-import {FaSearch} from 'react-icons/fa'
+
+import { FaSearch } from 'react-icons/fa';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
-
+import { useEffect, useState } from 'react';
 
 export default function Header()  {
 
   const { currentUser } = useSelector((state) => state.user);
-  
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <header className='bg-orange-200 shadow-xl'>
    <div className='flex justify-between items-center max-w-6xl mx-auto pt-3 pb-3'>
@@ -17,21 +32,26 @@ export default function Header()  {
         <span className='text-slate-700'>Estate</span>
     </h1>
    </Link>
-    <form className='bg-slate-100 p-3 rounded-lg flex text-sm md:text-md'>
-      <input type="text" placeholder='Search.....' className="bg-transparent focus:outline-none w-24 sm:w-64"/>
+    <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex text-sm md:text-md'>
+      <input type="text" placeholder='Search.....' className="bg-transparent focus:outline-none w-24 sm:w-64 font-bold" value={searchTerm} onChange={(e)=>{
+setSearchTerm(e.target.value);
+      }}/>
+      <button>
       <FaSearch className='bg-slate-500 bg-transparent mt-1'/>
+
+      </button>
     </form>
-    <ul className='flex gap-3'>
+    <ul className='flex gap-5 text-lg '>
       <Link to='/'>
-      <li className='hidden sm:inline text-slate-800 hover:underline'>Home</li>
+      <li className='hidden sm:inline text-lg  font-semibold text-slate-800 hover:underline'>Home</li>
       </Link>
 <Link to='/about'>
-      <li className='hidden sm:inline text-slate-800 hover:underline'>About</li>
+      <li className='hidden sm:inline text-lg font-semibold text-slate-800 hover:underline'>About</li>
 </Link>
 <Link to ='/profile'>
   {
   currentUser? (<img
-    className='rounded-full h-7 w-7 object-cover'
+    className='rounded-full h-10 w-10 object-cover'
     src={currentUser.avatar}
     alt='profile'
     />) : 
