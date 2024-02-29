d2d03b2ec3d67e63dfdf9daf35a50134c865c1d1
swis saklo <swissaklo@gmail.com>
2024-02-29 14:29:05 +0530

    css changes

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -3,7 +3,7 @@ import { MdLocationOn } from 'react-icons/md';
 
 export default function ListingItem({ listing }) {
   return (
-    <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[300px]'>
+    <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[270px]'>
       <Link to={`/listing/${listing._id}`}>
         <img
           src={
@@ -14,7 +14,7 @@ export default function ListingItem({ listing }) {
           className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300'
         />
         <div className='p-3 flex flex-col gap-2 w-full'>
-          <p className='truncate text-lg font-semibold text-slate-700'>
+          <p className='truncate text-lg font-semibold text-black-800'>
             {listing.name}
           </p>
           <div className='flex items-center gap-1'>
